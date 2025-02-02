1808939865ff92f6e5f0b600c816c5a236058ca5
mm0177 <113229869+mm0177@users.noreply.github.com>
2025-02-02 16:14:51 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -2,13 +2,7 @@ import * as firebase from 'firebase';
 import 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBnLUHidIafVYqJ4nwPGn_uK5lLriGokwE",
-    authDomain: "react-quick-food.firebaseapp.com",
-    databaseURL: "https://react-quick-food.firebaseio.com",
-    projectId: "react-quick-food",
-    storageBucket: "react-quick-food.appspot.com",
-    messagingSenderId: "775496944172",
-    appId: "1:775496944172:web:c57dc1dbd0aa26dd"
+   
 };
 
 // Initialize Firebase
